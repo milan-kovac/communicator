655f4dba91c06a20897a39ff97b9eae3f47eb08e
Milan Kovac <113798238+milan-kovac@users.noreply.github.com>
2023-01-28 12:48:44 +0100

    Initital commit

---
+++ communicator_app/android/app/src/main/kotlin/com/example/communicator_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.communicator_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ communicator_app/lib/utils/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/animation.dart';
+
+class AppColors {
+  static const backgroundColor = Color(0xFFfcf5f3);
+  static const buttonColor = Color(0xffeba049);
+}
